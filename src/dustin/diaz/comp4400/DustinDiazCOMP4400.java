a53549ef20f211f8ffd6e5568ea67e9d5c6e4b04
SWIFTKENKA <hunterghost526@gmail.com>
2020-03-25 01:57:08 -0400

    screen

--- src/dustin/diaz/comp4400/DustinDiazCOMP4400.java
+++ src/dustin/diaz/comp4400/DustinDiazCOMP4400.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.sql.DriverManager;
@@ -18,9 +19,14 @@ public class DustinDiazCOMP4400 extends Application {
 
     public static final String USERNAME = "root";
     public static final String PASSWORD = "s0m3t1m3s1h@t3p@ssw0rds";
+    private static final int MIN_WIDTH = 1100;
+    private static final int MIN_HEIGHT = 900;
+    private static int WIDTH = MIN_WIDTH;
+    private static int HEIGHT = MIN_HEIGHT;
+
 
     public DustinDiazCOMP4400() {
-        Computer.favicon = new Image(new File("src/Images/icons/favicon/android-chrome-192x192.png").toURI().toString());
+        Computer.favicon = new Image(new File("src/Images/icons/favicon/favicon-32x32.png").toURI().toString());
         Computer.src = new File("src").getAbsolutePath();
         Computer.pathChar = Computer.src.contains("\\") ? "\\" : "/";
         Computer.movieImagePath = Computer.src + Computer.pathChar + "Images" + Computer.pathChar + "movies" + Computer.pathChar;
@@ -39,6 +45,33 @@ public class DustinDiazCOMP4400 extends Application {
     public static Scene scene;
     public static Stage stage;
 
+    private static void setScreen() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int w = screenSize.width;
+        int h = screenSize.height;
+
+        if (w >= 3440) {
+            w /= 3;
+        } else if (w >= 2560) {
+            w -= MIN_WIDTH;
+            w -= (2560 - MIN_WIDTH) * 2;
+        } else if (w >= 1920) {
+            w -= (1920 - MIN_WIDTH);
+        } else {
+            w -= 10;
+        }
+
+        if (h < MIN_HEIGHT) {
+            h -= 10;
+        } else {
+            h = MIN_HEIGHT;
+        }
+
+        WIDTH = w;
+        HEIGHT = h;
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         connect.start();
@@ -50,11 +83,11 @@ public class DustinDiazCOMP4400 extends Application {
         stage.setTitle("Dustin Díaz (A00548394) COMP4400 - Movie Rental System");
         stage.getIcons().add(Computer.favicon);
 
-        stage.setMinHeight(900);
-        stage.setMinWidth(1100);
-
-        stage.setHeight(1030);
-        stage.setWidth(1300);
+        stage.setMinHeight(MIN_HEIGHT);
+        stage.setMinWidth(MIN_WIDTH);
+        setScreen();
+        stage.setHeight(HEIGHT);
+        stage.setWidth(WIDTH);
 
         stage.setOnCloseRequest(e -> {
             e.consume();
